aedb6398ffd5b7f971f3318a54f39e93f85b9c65
Brett Huber <bretthuber6@gmail.com>
2018-02-03 14:36:53 -0600

    Setup CI (#7)
    * add config file for travis CI
    
    * modify travis ci config
    
    * modify travis ci config
    
    * modify travis ci config
    
    * attempt to fix failing travis CI Build
    
    * attempt to fix failing travis CI Build

--- src/components/Spendatures/MonthlyTransactions/index.js
+++ src/components/Spendatures/MonthlyTransactions/index.js
@@ -1,11 +1,11 @@
 import { h } from 'preact';
 import style from './style.css';
-import Textfield from 'preact-material-components/Textfield';
-import 'preact-material-components/Textfield/style.css';
+import TextField from 'preact-material-components/TextField';
+import 'preact-material-components/TextField/style.css';
 
 const MonthlyTransactions = ({ monthlyTransactions, handleMonthlyTransactionChange }) => (
 	<div id='spendature7' class={style.spendature}>
-		<Textfield multiline={false}
+		<TextField multiline={false}
 			value={monthlyTransactions} onKeyUp={handleMonthlyTransactionChange}
 			label="CC Transactions"
 			type="number"
--- src/components/Spendatures/SpendatureItem/index.js
+++ src/components/Spendatures/SpendatureItem/index.js
@@ -1,11 +1,11 @@
 import { h } from 'preact';
-import Textfield from 'preact-material-components/Textfield';
-import 'preact-material-components/Textfield/style.css';
+import TextField from 'preact-material-components/TextField';
+import 'preact-material-components/TextField/style.css';
 import style from './style.css';
 
 const SpendatureItem = ({ id, category, monthlyValue, yearlyValue, handleMonthlyValueChange }) => (
 	<div id={'spendature' + id} class={style.spendature}>
-		<Textfield multiline={false}
+		<TextField multiline={false}
 			value={monthlyValue} onKeyUp={handleMonthlyValueChange}
 			label={category} id={id}
 			type="number"
