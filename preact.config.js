69fa574ef97dd42865c7cbf21f3b1fc16b50c2d5
Brett Huber <bretthuber6@gmail.com>
2018-02-10 11:45:07 -0600

    minify CreditCards.json for builds

--- preact.config.js
+++ preact.config.js
@@ -1,5 +1,14 @@
 import CopyWebpackPlugin from 'copy-webpack-plugin';
+import jsonminify from 'jsonminify';
 
 export default (config, env, helpers) => {
-	config.plugins.push( new CopyWebpackPlugin([{ context: `${__dirname}/src/Utils`, from: `CreditCards.json` }]) );
+	config.plugins.push( new CopyWebpackPlugin(
+		[{ 
+			context: `${__dirname}/src/Utils`, 
+			from: `CreditCards.json`, 
+			transform (content, path) {
+				//console.log(content.toString());
+        		return JSON.minify(content.toString());
+			  } 
+		}]) );
 };
