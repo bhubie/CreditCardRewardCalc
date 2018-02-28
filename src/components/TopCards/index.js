ce3bb0f21f9951582a684f3e771535d302ba8d91
Brett Huber <bretthuber6@gmail.com>
2018-02-27 22:46:12 -0600

    add new view all button

--- src/components/TopCards/index.js
+++ src/components/TopCards/index.js
@@ -1,12 +1,20 @@
 import { h } from 'preact';
 import style from './style.css';
+import 'preact-material-components/Button/style.css';
 import { TopCard } from './TopCard/index.js';
 
 const TopCards = (props) => (
-    <div id='topCardsContainer' class={style.topCardContainer} >
-        <TopCard creditCard={props.bestCard} id='bestCard'/>
-        <TopCard creditCard={props.secondBestCard} id='secondBestCard'/>
-        <TopCard creditCard={props.thirdBestCard} id='thirdBestCard'/>
+    <div class= {style.topCardContainer}> 
+        <div id='topCardsContainer' class={style.topCards} >
+            <TopCard creditCard={props.bestCard} id='bestCard'/>
+            <TopCard creditCard={props.secondBestCard} id='secondBestCard'/>
+            <TopCard creditCard={props.thirdBestCard} id='thirdBestCard'/>
+        </div>
+        <div class={style.button}>
+                <button class='mdc-button mdc-button--raised mdc-theme--secondary-bg'>
+                    VIEW ALL CARDS
+                </button>
+            </div>
     </div>
 );
 
