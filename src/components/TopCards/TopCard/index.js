b3734c69801429050d02d9d42e4dff4399255c10
Brett Huber <bretthuber6@gmail.com>
2018-02-28 23:46:11 -0600

    add new medal icons

--- src/components/TopCards/TopCard/index.js
+++ src/components/TopCards/TopCard/index.js
@@ -1,15 +1,35 @@
 import { h } from 'preact';
 import style from './style.css';
 import { formatAsCurrency } from '../../../Utils/Utils.js';
+import { IconMedalOne, IconMedalSecond, IconMedalThird } from '../../Icons/IconMedals/IconMedals.js'
 import Card from 'preact-material-components/Card';
 import 'preact-material-components/Button/style.css';
 import 'preact-material-components/Theme/style.css';
 
+const renderIcon = (rank) => {
+    switch(rank) {
+        case 1:
+            return <IconMedalOne />
+            break;
+        case 2:
+            return <IconMedalSecond />
+            break;
+        default:
+            return <IconMedalThird />
+    }
+}
 const TopCard = (props) => (
     <div id={props.id} class={style.topCard}>
-        <section class={style.cardHeader} > 
-            <h1 class={style.cardTitle}>{(props.creditCard != undefined) ? props.creditCard.RankWording : ''}</h1>
-            <h2 class={style.cardSubTitle}>{(props.creditCard != undefined) ? props.creditCard.Name : ''}</h2>
+        <section class={style.cardHeader}>
+            <div class= {style.headerContainer}> 
+                <div id='labelContainer'>
+                    <h1 class={style.cardTitle}>{(props.creditCard != undefined) ? props.creditCard.RankWording : ''}</h1>
+                    <h2 class={style.cardSubTitle}>{(props.creditCard != undefined) ? props.creditCard.Name : ''}</h2>
+                </div>
+                <div id='iconContainer'>
+                {(props.creditCard != undefined) ? renderIcon(props.creditCard.Rank) : '' }
+                </div>
+            </div>
         </section>
         <section id='cardBody' class={style.cardBody}>
             <div id='itemCardType' class={style.itemContainer}>
